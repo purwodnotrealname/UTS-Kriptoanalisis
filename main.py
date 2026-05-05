0f30c44b64460f69bac2bffe6ae5b1faa4b6b309
nara <putuaryanaradhatkj@gmail.com>
2026-05-05 20:34:11 +0800

    LGTM fixing key

--- main.py
+++ main.py
@@ -14,23 +14,22 @@ def main():
     plaintext_bytes = plaintext_raw.encode('utf-8') # encode string ke bytes
     
     key = "NT" # hardcoded key panjang 2 bytes
-    key = int.from_bytes(key.encode('utf-8'), byteorder='big') # string to integer
+    key_bytes = key.encode('utf-8') # string to bytes
 
     if mode_choice == '1': # penyelesaian soal no 1
-        encrypt_result = xor_encrypt(plaintext_bytes, key)
+        encrypt_result = xor_encrypt(plaintext_bytes, key_bytes)
         ciphertext = encrypt_result
         print(" ".join(ciphertext))
 
-        decrypt_result = xor_decrypt(ciphertext, key)
+        decrypt_result = xor_decrypt(ciphertext, key_bytes)
         print("Decrypted Text:", decrypt_result)
 
     elif mode_choice == '2':
-        caesar_encrypt_result = caesar_encrypt(plaintext_raw, 3) # 
+        caesar_encrypt_result = caesar_encrypt(plaintext_raw, 3) # shift 3 kali
         print("Caesar Encrypted Text:", caesar_encrypt_result)
 
         caesar_decrypt_result = caesar_decrypt(caesar_encrypt_result, 3)
         print("Caesar Decrypted Text:", caesar_decrypt_result)
-
 
     elif mode_choice == '3':
         print("unassigned command")
--- operations_1.py
+++ operations_1.py
@@ -1,13 +1,16 @@
-def xor_encrypt(plaintext, key): #plaintext in in binary string format, key integer format
+def xor_encrypt(plaintext, key_bytes): 
     ciphertext = [] #store ciphertext as list of binary string format
     for i in range(len(plaintext)):
-        cipher_char = plaintext[i] ^ key # int and int XOR operation
-        ciphertext.append(bin(cipher_char))
+        key_byte = key_bytes[i % len(key_bytes)] 
+        cipher_char = plaintext[i] ^ key_byte
+        ciphertext.append(format(cipher_char, '#010b')) 
     return ciphertext
 
-def xor_decrypt(ciphertext, key): #ciphertext in list of binary string format, key integer format
+
+def xor_decrypt(ciphertext, key_bytes): #ciphertext in list of binary string format, key_bytes bytes format
     decrypted_text = [] #store decrypted characters as integers
-    for cipher_char in ciphertext:
-        decrypted_char = int(cipher_char, 2) ^ key
+    for i, cipher_char in enumerate(ciphertext):
+        key_byte = key_bytes[i % len(key_bytes)] 
+        decrypted_char = int(cipher_char, 2) ^ key_byte
         decrypted_text.append(decrypted_char)
     return bytes(decrypted_text).decode('utf-8')
